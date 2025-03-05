2c23d7675503825a0844bb315b3270632f32956c
Jeroen <info@joomill.nl>
2025-03-05 07:54:20 +0100

    v5.1.0 - PRO Only language string

--- elements/pro.php
+++ elements/pro.php
@@ -21,7 +21,7 @@ class JFormFieldPRO extends ListField
 
 	protected function getInput()
 	{
-		$text = Text::_('PLG_FIELDS_USERDROPDOWN_PARAMS_PRO_ONLY');
+		$text = Text::_('PLG_FIELDS_USERDROPDOWN_PRO_ONLY');
 
 		return
 			'<code>' . $text . '</code>';
